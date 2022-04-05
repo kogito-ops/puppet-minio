b6e4b054780a748fa9305803d6a9c6794f227d9a
jonasdemoor <17252323+jonasdemoor@users.noreply.github.com>
2022-04-05 18:52:28 +0200

    lib/puppet_x/minio/client: Add helper method for using default alias

--- lib/puppet_x/minio/client.rb
+++ lib/puppet_x/minio/client.rb
@@ -6,8 +6,10 @@ module PuppetX # rubocop:disable Style/ClassAndModuleChildren
   module Minio # rubocop:disable Style/ClassAndModuleChildren
     class Client # rubocop:disable Style/Documentation
       CLIENT_LINK_LOCATION = '/root/.minioclient'
+      DEFAULT_ALIAS_LOCATION = '/root/.minio_default_alias'
 
       @client_location = nil
+      @default_alias = nil
 
       def self.execute(args, **execute_args)
         ensure_client_installed
@@ -17,6 +19,11 @@ module PuppetX # rubocop:disable Style/ClassAndModuleChildren
         out.each_line.map do |line|
           JSON.parse(line)
         end
+      end
+
+      def self.alias
+        ensure_alias
+        @default_alias
       end
 
       def self.ensure_client_installed
@@ -33,8 +40,26 @@ module PuppetX # rubocop:disable Style/ClassAndModuleChildren
         @client_location = File.readlink(CLIENT_LINK_LOCATION)
       end
 
+      def self.ensure_alias
+        return if @default_alias
+
+        unless alias_set?
+          errormsg = [
+            "MinIO default alias file does not exist at #{DEFAULT_ALIAS_LOCATION}. ",
+            'Make sure to specify an alias to be used with `minio::default_client_alias`.',
+          ]
+          raise Puppet::ExecutionFailure, errormsg.join
+        end
+
+        @default_alias = File.read(DEFAULT_ALIAS_LOCATION)
+      end
+
       def self.installed?
         File.exist?(CLIENT_LINK_LOCATION)
+      end
+
+      def self.alias_set?
+        File.exist?(DEFAULT_ALIAS_LOCATION)
       end
     end
   end
